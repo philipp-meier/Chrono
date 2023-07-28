c9f655198feb254cdef85ec49fc6028110ebd372
Philipp Meier <philipp.meier@gmx.at>
2023-07-28 13:42:55 +0200

    refactor: MasterDataPage.tsx

--- src/WebUI/ClientApp/src/presentation/pages/MasterDataPage.tsx
+++ src/WebUI/ClientApp/src/presentation/pages/MasterDataPage.tsx
@@ -11,17 +11,25 @@ interface MasterDataPageState {
   categories: MasterDataItem[];
 }
 
+interface TabPaneRenderOptions {
+  title: string;
+  items: MasterDataItem[];
+  createCallback: (title: string) => Promise<number>;
+  deleteCallback: (id: number) => Promise<boolean>;
+  editModal?: any;
+}
+
 export default class MasterDataPage extends React.Component<any, MasterDataPageState> {
   constructor(props: any) {
     super(props);
     this.state = {taskLists: [], categories: []};
   }
 
-  componentDidMount(): void {
-    this.refreshDataAsync();
+  async componentDidMount(): Promise<void> {
+    await this.refreshMasterDataAsync();
   }
 
-  async refreshDataAsync() {
+  async refreshMasterDataAsync(): Promise<void> {
     const taskLists = (await getTaskLists()).map((x) => {
       return {id: x.id, name: x.title};
     });
@@ -33,76 +41,89 @@ export default class MasterDataPage extends React.Component<any, MasterDataPageS
   }
 
   render() {
-    const panes = [
+    return (
+      <Tab
+        menu={{attached: true, tabular: false}}
+        panes={this.buildTabPanes()}
+        className="masterData tabs"
+      />
+    );
+  }
+
+  buildTabPanes() {
+    return [
       {
         menuItem: "Task lists",
-        render: () => (
-          <Tab.Pane>
-            <MasterDataListControl
-              itemTitle="Task List"
-              items={this.state.taskLists.sort((a, b) => a.name.localeCompare(b.name))}
-              addModal={<AddItemModal itemTitle="Task List" onAdd={(title: string) => {
-                createTaskList(title).then((id) => {
-                  if (id > 0) {
-                    this.state.taskLists.push({id: id, name: title});
-                    this.setState({taskLists: this.state.taskLists, categories: this.state.categories});
-                  }
-                });
-              }}/>}
-              editModal={<TaskListEditModal/>}
-              onDelete={(item) => {
-                const index = this.state.taskLists.indexOf(item);
-                if (index >= 0) {
-                  deleteTaskList(item.id).then((isDeleted) => {
-                    if (isDeleted) {
-                      this.state.taskLists.splice(index, 1);
-                      this.setState({taskLists: this.state.taskLists, categories: this.state.categories});
-                    }
-                  });
-                }
-              }}
-              refreshDataCallback={() => this.refreshDataAsync()}
-            />
-          </Tab.Pane>
-        ),
+        render: this.renderTabPane({
+          title: "Task List",
+          items: this.state.taskLists,
+          createCallback: createTaskList,
+          deleteCallback: deleteTaskList,
+          editModal: <TaskListEditModal/>
+        }),
       },
       {
         menuItem: "Categories",
-        render: () => (
-          <Tab.Pane>
-            <MasterDataListControl
-              itemTitle="Category"
-              items={this.state.categories.sort((a, b) => a.name.localeCompare(b.name))}
-              addModal={<AddItemModal itemTitle="Category" onAdd={(title: string) => {
-                createCategory(title).then((id) => {
-                  if (id > 0) {
-                    this.state.categories.push({id: id, name: title});
-                    this.setState({taskLists: this.state.taskLists, categories: this.state.categories});
-                  }
-                });
-              }}/>}
-              onDelete={(item) => {
-                const index = this.state.categories.indexOf(item);
-                if (index >= 0) {
-                  deleteCategory(item.id).then((isDeleted) => {
-                    if (isDeleted) {
-                      this.state.categories.splice(index, 1);
-                      this.setState({taskLists: this.state.taskLists, categories: this.state.categories});
-                    }
-                  });
-                }
-              }}
-            />
-          </Tab.Pane>
-        ),
+        render: this.renderTabPane({
+          title: "Category",
+          items: this.state.categories,
+          createCallback: createCategory,
+          deleteCallback: deleteCategory
+        }),
       },
-    ];
+    ]
+  }
 
-    return (
-      <Tab
-        menu={{attached: true, tabular: false}} panes={panes}
-        className="masterData tabs"
-      />
+  renderTabPane(options: TabPaneRenderOptions) {
+    return () => (
+      <Tab.Pane>
+        <MasterDataListControl
+          itemTitle={options.title}
+          items={this.sortMasterDataItems(options.items)}
+          addModal={
+            <AddItemModal
+              itemTitle={options.title}
+              onAdd={(title: string) => this.createMasterDataItem(options.items, title, options.createCallback)}
+            />
+          }
+          editModal={options.editModal}
+          onDelete={(item: MasterDataItem) => this.deleteMasterDataItem(options.items, item, options.deleteCallback)}
+          refreshDataCallback={() => this.refreshMasterDataAsync()}
+        />
+      </Tab.Pane>
+    );
+  }
+
+  sortMasterDataItems(items: MasterDataItem[]) {
+    return items.sort((a, b) => a.name.localeCompare(b.name));
+  }
+
+  updateState() {
+    this.setState({taskLists: this.state.taskLists, categories: this.state.categories});
+  }
+
+  createMasterDataItem(items: MasterDataItem[], title: string, createCallback: (title: string) => Promise<number>) {
+    createCallback(title).then((id) => {
+        if (id <= 0)
+          return;
+
+        items.push({id: id, name: title});
+        this.updateState();
+      }
+    );
+  }
+
+  deleteMasterDataItem(items: MasterDataItem[], item: MasterDataItem, deleteCallback: (id: number) => Promise<boolean>) {
+    const index = items.indexOf(item);
+    if (index < 0)
+      return;
+
+    deleteCallback(item.id).then((isDeleted) => {
+        if (isDeleted) {
+          items.splice(index, 1);
+          this.updateState();
+        }
+      }
     );
   }
 }
