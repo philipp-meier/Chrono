933993853a2d93add515f34a563ccb5983ed378c
Philipp Meier <philipp@p-meier.dev>
2023-08-20 17:09:44 +0200

    feat: Switched to Vite (#19)
    * feat: Migrated to Vite

--- src/WebUI/ClientApp/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders learn react link", () => {
-  render(<App />);
-  const homeMenu = screen.getByText(/Home/i);
-  expect(homeMenu).toBeInTheDocument();
-});
