b130e93ad661474ac4b9c08d2b818ff89813250d
Philipp Meier <philipp.meier@gmx.at>
2023-09-20 23:15:38 +0200

    feat(tests): Added e2e tests.

---
+++ src/Chrono.Tests/E2E/02_Categories.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+
+namespace Chrono.Tests.E2E;
+
+public partial class E2ETests
+{
+    [Test] [Order(2)]
+    public async Task Create_Category()
+    {
+        await _page.GotoAsync(_config["WebAppUrl"]!);
+        await _page.Locator("text=Master Data").ClickAsync();
+        await _page.Locator("text=Categories").ClickAsync();
+        await _page.Locator("text=Add Category").ClickAsync();
+        await _page.GetByRole(AriaRole.Textbox).FillAsync("Test Category");
+
+        await _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions
+        {
+            Name = "Add", Exact = true
+        }).ClickAsync();
+    }
+}
---
+++ src/Chrono.Tests/E2E/03_Tasks.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+
+namespace Chrono.Tests.E2E;
+
+public partial class E2ETests
+{
+    [Test] [Order(3)]
+    public async Task Create_Task()
+    {
+        await _page.GotoAsync(_config["WebAppUrl"]!);
+        await _page.Locator("text=Lists").ClickAsync();
+        await _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions
+        {
+            Name = "Add Task", Exact = true
+        }).ClickAsync();
+
+        await _page.GetByRole(AriaRole.Textbox, new PageGetByRoleOptions
+        {
+            Name = "Name", Exact = true
+        }).FillAsync("Test Task");
+
+        await _page.GetByRole(AriaRole.Textbox, new PageGetByRoleOptions
+        {
+            Name = "Business value", Exact = true
+        }).FillAsync("QA");
+
+        await _page.GetByRole(AriaRole.Textbox, new PageGetByRoleOptions
+        {
+            Name = "Description", Exact = true
+        }).FillAsync("Test description");
+
+        await _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions
+        {
+            Name = "Save"
+        }).ClickAsync();
+    }
+}
