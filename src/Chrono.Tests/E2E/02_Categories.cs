9efb574e6ff24c99e1b03064291eccbc0ba2d1f3
Philipp Meier <philipp.meier@gmx.at>
2023-09-21 19:40:30 +0200

    feat: Added e2e tests.

--- src/Chrono.Tests/E2E/02_Categories.cs
+++ src/Chrono.Tests/E2E/02_Categories.cs
@@ -5,7 +5,7 @@ namespace Chrono.Tests.E2E;
 
 public partial class E2ETests
 {
-    [Test] [Order(2)]
+    [Test] [Order(4)]
     public async Task Create_Category()
     {
         await _page.GotoAsync(_config["WebAppUrl"]!);
@@ -17,6 +17,20 @@ public partial class E2ETests
         await _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions
         {
             Name = "Add", Exact = true
+        }).ClickAsync();
+    }
+
+    [Test] [Order(8)]
+    public async Task Delete_Category()
+    {
+        await _page.GotoAsync(_config["WebAppUrl"]!);
+        await _page.Locator("text=Master Data").ClickAsync();
+        await _page.Locator("text=Categories").ClickAsync();
+        await _page.Locator("button[data-delete='Test Category']").First.ClickAsync();
+
+        await _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions
+        {
+            Name = "OK", Exact = true
         }).ClickAsync();
     }
 }
