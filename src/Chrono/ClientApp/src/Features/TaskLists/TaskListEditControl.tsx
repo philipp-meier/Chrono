9d75ef0af035c58c3129f2ec65c768e917988dc7
Philipp Meier <philipp@p-meier.dev>
2024-03-27 19:27:50 +0100

    feat: Task list and category filters are now sorted.

--- src/Chrono/ClientApp/src/Features/TaskLists/TaskListEditControl.tsx
+++ src/Chrono/ClientApp/src/Features/TaskLists/TaskListEditControl.tsx
@@ -13,188 +13,191 @@ import NoItemsMessage from "../../Shared/Components/NoItemsMessage";
 import JSendApiClient, {API_ENDPOINTS} from "../../Shared/JSendApiClient";
 
 const TaskListEditControl = (props: { taskListId: number }) => {
-    const [taskList, setTaskList] = useState<TaskList | null>(null);
-    const [category, setCategory] = useState("");
-    const [doneFilter, setDoneFilter] = useState(false);
-    const [availableTaskLists, setAvailableTaskLists] = useState<TaskListBrief[]>([]);
-    const [availableCategories, setAvailableCategories] = useState<Category[]>([]);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const isMobileOptimized = useMediaQuery({query: "(max-width:682px)"});
+  const [taskList, setTaskList] = useState<TaskList | null>(null);
+  const [category, setCategory] = useState("");
+  const [doneFilter, setDoneFilter] = useState(false);
+  const [availableTaskLists, setAvailableTaskLists] = useState<TaskListBrief[]>([]);
+  const [availableCategories, setAvailableCategories] = useState<Category[]>([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const isMobileOptimized = useMediaQuery({query: "(max-width:682px)"});
 
-    useEffect(() => {
-        const dataFetch = async () => {
-            const taskLists = await JSendApiClient.get<TaskListBrief[]>(API_ENDPOINTS.TaskLists);
-            setAvailableTaskLists(taskLists ?? []);
+  useEffect(() => {
+    const dataFetch = async () => {
+      const taskLists = (await JSendApiClient.get<TaskListBrief[]>(API_ENDPOINTS.TaskLists) ?? [])
+        .sort((a, b) => a.title.localeCompare(b.title))
 
-            const categories = await JSendApiClient.get<Category[]>(API_ENDPOINTS.Categories);
-            setAvailableCategories(categories ?? []);
+      const categories = (await JSendApiClient.get<Category[]>(API_ENDPOINTS.Categories) ?? [])
+        .sort((a, b) => a.name.localeCompare(b.name))
 
-            if (!taskLists || taskLists.length === 0) {
-                setIsLoaded(true);
-                return;
-            }
+      setAvailableTaskLists(taskLists ?? []);
+      setAvailableCategories(categories ?? []);
 
-            const userSettings = await JSendApiClient.get<UserSettings>(API_ENDPOINTS.UserSettings);
+      if (!taskLists || taskLists.length === 0) {
+        setIsLoaded(true);
+        return;
+      }
 
-            const selectedTaskList = props.taskListId >= 0 ?
-                props.taskListId : userSettings?.defaultTaskListId;
+      const userSettings = await JSendApiClient.get<UserSettings>(API_ENDPOINTS.UserSettings);
 
-            const taskList = await JSendApiClient.get<TaskList>(`${API_ENDPOINTS.TaskLists}/${selectedTaskList ?? taskLists[0].id}`);
-            setTaskList(taskList);
-            setIsLoaded(true);
-        };
+      const selectedTaskList = props.taskListId >= 0 ?
+        props.taskListId : userSettings?.defaultTaskListId;
 
-        dataFetch()
-    }, [props.taskListId]);
-
-    const filteredTasks = (taskList?.tasks || [])
-        .filter((x) => x.done === doneFilter)
-        .filter(
-            (x) => !category || x.categories.map((x) => x.name).indexOf(category) >= 0
-        );
-
-    const [, setTasks] = useState(filteredTasks);
-    const handleTaskMove = (task: Task, direction: number) => {
-        JSendApiClient.update(`${API_ENDPOINTS.Tasks}/${task.id}`, {
-            id: task.id,
-            position: task.position + direction,
-            name: task.name,
-            businessValue: task.businessValue,
-            description: task.description,
-            done: task.done,
-            categories: task.categories,
-        }).then((isUpdated) => {
-            if (!isUpdated || Math.abs(direction) !== 1)
-                return;
-
-            const curIndex = filteredTasks.indexOf(task);
-            const isFirstPosMovingUp = curIndex === 0 && direction < 0;
-            const isLastPosMovingDown = curIndex === filteredTasks.length - 1 && direction > 0;
-
-            if (isFirstPosMovingUp || isLastPosMovingDown)
-                return;
-
-            filteredTasks[curIndex].position += direction;
-            filteredTasks[curIndex + direction].position -= direction;
-
-            setTasks([...filteredTasks]);
-        });
+      const taskList = await JSendApiClient.get<TaskList>(`${API_ENDPOINTS.TaskLists}/${selectedTaskList ?? taskLists[0].id}`);
+      setTaskList(taskList);
+      setIsLoaded(true);
     };
 
-    const taskListItems = filteredTasks
-        .sort((a, b) => a.position - b.position)
-        .map((task: Task) => (
-            <TaskItemViewControl
-                key={task.id}
-                task={task}
-                moveUp={
-                    filteredTasks.indexOf(task) !== 0
-                        ? () => handleTaskMove(task, -1)
-                        : undefined
-                }
-                moveDown={
-                    filteredTasks.indexOf(task) !== filteredTasks.length - 1
-                        ? () => handleTaskMove(task, +1)
-                        : undefined
-                }
-            />
-        ));
+    dataFetch()
+  }, [props.taskListId]);
 
-    return (
-        <Container className={isMobileOptimized ? "tasklist mobile" : "tasklist"}>
-            <Container
-                textAlign="right"
-                style={{marginTop: "1em"}}
-                className="tasklist-menu"
-            >
-                <div
-                    className={!isMobileOptimized ? "filters" : "filters ui form mini"}
-                >
-                    {!isMobileOptimized && (
-                        <Checkbox
-                            label="Done"
-                            checked={doneFilter}
-                            toggle
-                            style={{marginRight: "0.5em"}}
-                            onClick={() => {
-                                setDoneFilter(!doneFilter);
-                            }}
-                        />
-                    )}
-                    <Dropdown
-                        placeholder="Categories"
-                        search={!isMobileOptimized}
-                        clearable
-                        selection
-                        options={availableCategories.map((x) => {
-                            return {key: x.name, text: x.name, value: x.name};
-                        })}
-                        style={{marginRight: "0.5em"}}
-                        onChange={(e: any) => {
-                            setCategory(e.target.innerText);
-                        }}
-                    />
-                    <Dropdown
-                        placeholder={taskList ? taskList.title : "List"}
-                        search={!isMobileOptimized}
-                        selection
-                        options={availableTaskLists.map((x) => {
-                            return {key: x.id, text: x.title, value: x.title};
-                        })}
-                        style={{marginRight: "0.5em"}}
-                        defaultValue={taskList ? taskList.title : undefined}
-                        onChange={async (e: any) => {
-                            const selectedId = availableTaskLists.find(
-                                (x) => x.title === e.target.innerText
-                            )?.id;
-                            if (selectedId && selectedId !== taskList?.id) {
-                                setTaskList(await JSendApiClient.get<TaskList>(`${API_ENDPOINTS.TaskLists}/${selectedId}`));
-                            }
-                        }}
-                    />
-                    {isMobileOptimized && (
-                        <Checkbox
-                            label="Done"
-                            checked={doneFilter}
-                            toggle
-                            style={{marginRight: "0.5em", marginTop: "0.5em"}}
-                            onClick={() => {
-                                setDoneFilter(!doneFilter);
-                            }}
-                            className="ui form mini"
-                        />
-                    )}
-                </div>
-                <div className="buttons">
-                    <Button
-                        as="a"
-                        href={`/lists/${taskList?.id}/tasks/add`}
-                        circular={isMobileOptimized}
-                        size={isMobileOptimized ? "huge" : undefined}
-                        icon={isMobileOptimized}
-                        disabled={!taskList}
-                        primary
-                    >
-                        {isMobileOptimized ? <Icon name="add"/> : "Add Task"}
-                    </Button>
-                </div>
-            </Container>
-            {taskListItems.length > 0 &&
-                <Container className="ui divided tasklist-items items list">{taskListItems}</Container>}
-            {isLoaded && !taskList &&
-                <NoItemsMessage
-                    text={"Create your first task list in the \"Master Data\" menu."}
-                    buttonOptions={{text: "Open Master Data", href: "/masterdata"}}
-                />
-            }
-            {isLoaded && taskList && taskListItems.length == 0 &&
-                <NoItemsMessage
-                    text={`The list "${taskList.title}" does not contain any ${doneFilter ? 'completed' : 'open'} tasks at the moment.`}
-                    buttonOptions={!doneFilter ? {text: "Add a task", href: `/lists/${taskList?.id}/tasks/add`} : undefined}
-                />
-            }
-        </Container>
+  const filteredTasks = (taskList?.tasks || [])
+    .filter((x) => x.done === doneFilter)
+    .filter(
+      (x) => !category || x.categories.map((x) => x.name).indexOf(category) >= 0
     );
+
+  const [, setTasks] = useState(filteredTasks);
+  const handleTaskMove = (task: Task, direction: number) => {
+    JSendApiClient.update(`${API_ENDPOINTS.Tasks}/${task.id}`, {
+      id: task.id,
+      position: task.position + direction,
+      name: task.name,
+      businessValue: task.businessValue,
+      description: task.description,
+      done: task.done,
+      categories: task.categories,
+    }).then((isUpdated) => {
+      if (!isUpdated || Math.abs(direction) !== 1)
+        return;
+
+      const curIndex = filteredTasks.indexOf(task);
+      const isFirstPosMovingUp = curIndex === 0 && direction < 0;
+      const isLastPosMovingDown = curIndex === filteredTasks.length - 1 && direction > 0;
+
+      if (isFirstPosMovingUp || isLastPosMovingDown)
+        return;
+
+      filteredTasks[curIndex].position += direction;
+      filteredTasks[curIndex + direction].position -= direction;
+
+      setTasks([...filteredTasks]);
+    });
+  };
+
+  const taskListItems = filteredTasks
+    .sort((a, b) => a.position - b.position)
+    .map((task: Task) => (
+      <TaskItemViewControl
+        key={task.id}
+        task={task}
+        moveUp={
+          filteredTasks.indexOf(task) !== 0
+            ? () => handleTaskMove(task, -1)
+            : undefined
+        }
+        moveDown={
+          filteredTasks.indexOf(task) !== filteredTasks.length - 1
+            ? () => handleTaskMove(task, +1)
+            : undefined
+        }
+      />
+    ));
+
+  return (
+    <Container className={isMobileOptimized ? "tasklist mobile" : "tasklist"}>
+      <Container
+        textAlign="right"
+        style={{marginTop: "1em"}}
+        className="tasklist-menu"
+      >
+        <div
+          className={!isMobileOptimized ? "filters" : "filters ui form mini"}
+        >
+          {!isMobileOptimized && (
+            <Checkbox
+              label="Done"
+              checked={doneFilter}
+              toggle
+              style={{marginRight: "0.5em"}}
+              onClick={() => {
+                setDoneFilter(!doneFilter);
+              }}
+            />
+          )}
+          <Dropdown
+            placeholder="Categories"
+            search={!isMobileOptimized}
+            clearable
+            selection
+            options={availableCategories.map((x) => {
+              return {key: x.name, text: x.name, value: x.name};
+            })}
+            style={{marginRight: "0.5em"}}
+            onChange={(e: any) => {
+              setCategory(e.target.innerText);
+            }}
+          />
+          <Dropdown
+            placeholder={taskList ? taskList.title : "List"}
+            search={!isMobileOptimized}
+            selection
+            options={availableTaskLists.map((x) => {
+              return {key: x.id, text: x.title, value: x.title};
+            })}
+            style={{marginRight: "0.5em"}}
+            defaultValue={taskList ? taskList.title : undefined}
+            onChange={async (e: any) => {
+              const selectedId = availableTaskLists.find(
+                (x) => x.title === e.target.innerText
+              )?.id;
+              if (selectedId && selectedId !== taskList?.id) {
+                setTaskList(await JSendApiClient.get<TaskList>(`${API_ENDPOINTS.TaskLists}/${selectedId}`));
+              }
+            }}
+          />
+          {isMobileOptimized && (
+            <Checkbox
+              label="Done"
+              checked={doneFilter}
+              toggle
+              style={{marginRight: "0.5em", marginTop: "0.5em"}}
+              onClick={() => {
+                setDoneFilter(!doneFilter);
+              }}
+              className="ui form mini"
+            />
+          )}
+        </div>
+        <div className="buttons">
+          <Button
+            as="a"
+            href={`/lists/${taskList?.id}/tasks/add`}
+            circular={isMobileOptimized}
+            size={isMobileOptimized ? "huge" : undefined}
+            icon={isMobileOptimized}
+            disabled={!taskList}
+            primary
+          >
+            {isMobileOptimized ? <Icon name="add"/> : "Add Task"}
+          </Button>
+        </div>
+      </Container>
+      {taskListItems.length > 0 &&
+          <Container className="ui divided tasklist-items items list">{taskListItems}</Container>}
+      {isLoaded && !taskList &&
+          <NoItemsMessage
+              text={"Create your first task list in the \"Master Data\" menu."}
+              buttonOptions={{text: "Open Master Data", href: "/masterdata"}}
+          />
+      }
+      {isLoaded && taskList && taskListItems.length == 0 &&
+          <NoItemsMessage
+              text={`The list "${taskList.title}" does not contain any ${doneFilter ? 'completed' : 'open'} tasks at the moment.`}
+              buttonOptions={!doneFilter ? {text: "Add a task", href: `/lists/${taskList?.id}/tasks/add`} : undefined}
+          />
+      }
+    </Container>
+  );
 };
 
 export default TaskListEditControl;
